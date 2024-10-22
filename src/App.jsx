c31dc98dd9a160117d4273f719d7ea5301dc78c8
Nelly-B <alapanelly@gmail.com>
2024-10-22 06:01:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import AdminSta from './Components/AdminSta'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <AdminSta />
+      </div>
+    </>
+  )
+}
+
+export default App
